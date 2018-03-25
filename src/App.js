1db6647b7ff29a9f1e147e6633d944f0c38456f0
Carlos Guisao Franco <titofranco@gmail.com>
2018-03-25 17:03:22 -0500

    Add Person functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <Person />
       </div>
     );
   }
