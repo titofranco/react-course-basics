8e9cf52fce4118d94ede735a1f8f099c2211101f
Carlos Guisao Franco <titofranco@gmail.com>
2018-03-25 17:16:36 -0500

    Use properties in Person comp

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person />
+        <Person name='Carlos' age='32'/>
+        <Person name='Vivi' age='34'>Fashionist :v</Person>
+
       </div>
     );
   }
