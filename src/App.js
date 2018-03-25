4eb6fe379dad70e0c85a16162a0a5141122eee7f
Carlos Guisao Franco <titofranco@gmail.com>
2018-03-25 17:47:57 -0500

    Use setState to change state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import Person from './Person/Person.js';
 class App extends Component {
   // state: Only available if you extend from component
   // Should not be changed, if you change it, it will rerender the DOM
+  // You can of course pass the state  down to functional components, but these then can't directly edit it.
   state =  {
     people: [
       {name: 'Carlos', age: '32'},
       {name: 'Vivi', age: '34' }
-    ]
+    ],
+    hobbies: ['Run', 'Read']
+  }
+
+  // Use ES6 syntax so this refers to the class
+  switchNameHandler = () => {
+    // DONT DO THIS -> this.state.people[1].name = 'Patri';
+    this.setState({
+      people: [
+        {name: 'Vaco', age: '5'},
+        {name: 'Axl', age: '3' }
+      ]
+    });
   }
 
   render() {
@@ -17,9 +30,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}>Fashionist :v</Person>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>Hobbie is to: {this.state.hobbies[1]}</Person>
       </div>
     );
   }
