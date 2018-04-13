3e9002480b7b6e09650a5b55a786c42a7a188623
Carlos Guisao Franco <titofranco@gmail.com>
2018-04-12 20:28:07 -0500

    Use WithClass in Person.js

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import classes from './Person.css';
+import WithClass from '../../../hoc/WithClass';
 
 class Person extends Component {
   constructor(props) {
@@ -22,11 +23,11 @@ class Person extends Component {
   render() {
     console.log('[In Person.js] #render');
     return (
-      <div className={classes.Person}>
+      <WithClass classes={classes.Person}>
         <p onClick={this.props.myCustomClickEvent}>I'm a {this.props.name}! and I am { this.props.age } years old, click me! </p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.inputChanged} value={this.props.name}/>
-      </div>
+      </WithClass>
     );
   }
 }
